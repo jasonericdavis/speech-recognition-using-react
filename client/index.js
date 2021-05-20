c3ffda848d55119d22268689693f5b26e520ce1d
Jason Davis <14043845+jasonericdavis@users.noreply.github.com>
2021-05-20 13:59:29 -0400

    started working on adding socket.io

--- client/index.js
+++ client/index.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
+import Socket from './lib/socket'
 
+const socket = Socket();
 
-const App = () => <h1>Hello!!</h1>
+const App = () => {
+    const [connected, setConnected] = useState(false)
+
+    useEffect(() => {
+        setSocketState(socket.state)
+    }, [socket.state])
+
+    return (
+        <div>
+            <h1>Hello!!</h1>
+            <p>{socketState}</p>
+        </div>
+    )
+}
 
 ReactDOM.render(<App />,document.getElementById('app'));
