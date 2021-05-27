da5e35b540245d06ed113c7ba9b41a2fa4e87c31
Jason Davis <14043845+jasonericdavis@users.noreply.github.com>
2021-05-26 21:11:59 -0400

    By george I think it works

--- server.js
+++ server.js
@@ -1,20 +1,156 @@
-const app = require('express')()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const next = require('next')
-const StreamingClient = require('./lib/StreamingClient')
+const env = require('dotenv')
+const express = require('express')
+const http = require('http')
+const multer = require('multer')
+const { RevAiApiClient, CaptionType, RevAiStreamingClient, AudioConfig } = require('revai-node-sdk');
+const { Server } = require('socket.io')
 
+const app = express();
+const port = 3000;
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST"]
+  }
+});
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const nextApp = next({ dev })
-const nextHandler = nextApp.getRequestHandler()
-const streamingClient = new StreamingClient(process.env.REVAI_ACCESS_TOKEN, (data) => {
-  io.emit('transcript', data)
+env.config();
+const access_token = process.env.revai_access_token;
+const base_url = process.env.base_url || `http://localhost${port}`
+const media_path = process.env.media_path || 'public/media/'
+
+// setup multer to get file uploads
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, media_path)
+  },
+
+  // for this example we just want to use the same filename as before
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  }
+})
+const upload = multer({storage})
+
+// Setup the Rev.ai sdk
+const asyncClient = new RevAiApiClient(access_token);
+let streamingClient;
+
+// This middleware has to be called before the routes
+app.use(express.json());
+
+/**
+ *  Notes: There is currently a 10MB limit on the size of the audio file because 
+ * of a limitiation in axios with the maxBodyLength and maxContentLength
+ */
+app.post('/media', upload.single('mediaFile'), async (req, res, next) => {
+  const media_url = `${base_url}/media/${req.file.filename}`
+  const webhook_url = `${base_url}/job`;
+  console.log(`submit job: ${JSON.stringify({media_url, webhook_url})}`)
+  
+  try {
+    const job = await asyncClient.submitJobUrl(media_url, {
+      callback_url: webhook_url,
+    })
+    res.json(job);
+  } catch(err) {
+    console.dir(`Error: ${err.message}`)
+    res.status(500).json({error: err.message})
+  }
 })
 
-// socket.io server
-io.on('connection', socket => {
+app.post('/job', (req, res) => {
+  console.log(`webhook received: ${JSON.stringify(req.body)}`)
+  io.emit(`job`, req.body)
+  res.sendStatus(200)
+})
+
+app.get('/transcription/:jobId/:format', async (req, res) => {
+  try {
+    const {jobId, format} = req.params
+    console.dir({jobId, format})
+
+    if(format.toLowerCase() === 'json') {
+      const transcript = await asyncClient.getTranscriptObject(jobId)
+      res.json(transcript)
+      return
+    }
+
+    if(format.toLowerCase() === 'text') {
+      const transcript = await asyncClient.getTranscriptText(jobId)
+      res.send(transcript)
+      return
+    }
+    res.statusCode(500).send(`Invalid format ${format}`)
+  } catch(err) {
+    console.err(err.message)
+    res.sendStatus(500)
+  }
+})
+
+app.get('/caption/:jobId', async (req, res) => {
+  try {
+    const {jobId} = req.params
+    console.dir(jobId)
+    let output = '';
+    await asyncClient.getCaptions(jobId, CaptionType.VTT)
+    .then(response => {
+      const stream = response
+      stream.on('data', chunk => {
+        output += chunk
+      })
+
+      stream.on('end', () => {
+        console.log(output);
+        res.send(output)
+      })
+    })
+  } catch(err) {
+    console.error(err.message)
+    res.sendStatus(500)
+  }
+})
+
+app.post('/stream/start', (req, res) => {
+  console.log('Opening the stream')
+    streamingClient = new RevAiStreamingClient(
+      access_token, new AudioConfig('audio/x-wav')
+    )
+
+    streamingClient.on('close', (code, reason) => {
+      console.log(`Connection closed, ${code}: ${reason}`);
+    })
+
+    streamingClient.on('httpResponse', code => {
+      console.log(`Streaming client received http response with code: ${code}`);
+    })
+
+    streamingClient.on('connectFailed', error => {
+        console.log(`Connection failed with error: ${error}`);
+    })
+
+    streamingClient.on('connect', connectionMessage => {
+        console.log(`Connected with job id: ${connectionMessage.id}`);
+    })
+
+    revStream = streamingClient.start()
+    revStream.on('data', data => {
+      io.emit('transcript', data)
+    })
+    
+    res.sendStatus(200)
+})
+
+app.post('/stream/end', (req, res) => {
+  console.log('Closing stream')
+  revStream = null;
+  streamingClient.end();
+  streamingClient = null;
+  res.sendStatus(200)
+})
+
+io.on('connection', (socket) => {
   console.log(`connection made (${socket.id})`)
   
   socket.on('disconnect', () => {
@@ -22,32 +158,14 @@ io.on('connection', socket => {
   })
 
   socket.on('stream', data => {
-    streamingClient.stream(data)
-  })
-
-  socket.on('message',  message => {
-    console.log(`message recieved: ${message}`)
+    console.log('stream data received')
+    revStream && revStream.write(data)
   })
 })
 
-nextApp.prepare().then(() => {
-  app.use(function (req, res, next) {
-    req.io = io;
-    req.streamingClient = streamingClient
-    
-    next();
-  });
+app.use(express.static('public'))
+app.use('/media', express.static(media_path))
 
-  app.get('*', (req, res) => {
-    return nextHandler(req, res)
-  })
-
-  app.post('*', (req, res) => {
-    return nextHandler(req, res)
-  })
-
-  server.listen(port, err => {
-    if (err) throw err
-    console.log(`Ready on http://localhost:${port}`)
-  })
+server.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
 })
--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,14 +1,42 @@
+import proxy from 'http2-proxy';
 /** @type {import("snowpack").SnowpackUserConfig } */
+
 export default {
   mount: {
     /* ... */
+    public: '/',
+    src: '/dist'
   },
   plugins: [
     /* ... */
+    '@snowpack/plugin-postcss'
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    {
+      src: '/ws',
+      upgrade: (req, socket, head) => {
+        const defaultWSHandler = (err, req, socket, head) => {
+          if (err) {
+            console.error('proxy error', err);
+            socket.destroy();
+          }
+          console.log('proxy worked')
+        };
+
+        proxy.ws(
+          req,
+          socket,
+          head,
+          {
+            hostname: 'localhost',
+            port: 3000,
+          },
+          defaultWSHandler,
+        );
+      },
+    }
   ],
   optimize: {
     /* Example: Bundle your final build: */
@@ -19,8 +47,10 @@ export default {
   },
   devOptions: {
     /* ... */
+    tailwindConfig: './tailwind.config.js'
   },
   buildOptions: {
     /* ... */
   },
+  exclude: ['**/node_modules/**/*', 'server/**/*']
 };
